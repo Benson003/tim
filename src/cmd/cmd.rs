083a30adc6acbeaea020b711f7c8fa3809d0a753
Onyedika Nwankwo <nwankwobenson29@gmail.com>
2026-07-14 22:53:46 +0100

    fix: Fixed a race conditon of time of check time of use

--- src/cmd/cmd.rs
+++ src/cmd/cmd.rs
@@ -1,18 +1,11 @@
-use std::{env,fs,path::Path};
+use std::{env, fs};
 
-pub fn load_file_from_args()-> Result<String,String>{
+pub fn load_file_from_args() -> Result<String, String> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2{
-        return Err("Please provide a file".to_string());
-    }
 
-    let path = Path::new(&args[1]);
-    if !path.exists() || !path.is_file(){
+    let path = args
+        .get(1)
+        .ok_or_else(|| "Please provide a file".to_string())?;
 
-        return Err("Ïnvalid file path".to_string());
-    }
-    match fs::read_to_string(path){
-        Ok(content) => Ok(content),
-        Err(e) => Err(format!("Failed to read file: {}",e)),
-    }
+    fs::read_to_string(path).map_err(|e| format!("failed to read file:{e}"))
 }
